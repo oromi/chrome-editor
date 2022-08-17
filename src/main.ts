fd9f1788c105e8a7610c2aebce1ac9542e1c26ea
oromi <exemplumluna+git@gmail.com>
2022-08-17 19:27:46 +0900

    add function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import './style.css'
-
+import * as monaco from "monaco-editor";
 const app = document.querySelector<HTMLDivElement>('#app')!
+const editor = monaco.editor.create(app, {
+  fontSize: 18,
+  language: "markdown",
+  lineHeight: 1.6,
+  minimap: { enabled: false },
+  padding: { bottom: 16, top: 16},
+  theme:"vs-dark"
+});
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+editor.onDidChangeModelContent(() => {
+  const content = editor.getValue();
+});
